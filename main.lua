d36914403b3eef7875a0495dc13aeab7bc05bfb4
Traidepchuyenanh <dangbaohoanghuy0505@gmail.com>
2025-05-02 12:02:09 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,2 +1,3 @@
 print("evadehub loaded thành công!")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Traidepchuyenanh/evadehub/main/scriptdemo.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Traidepchuyenanh/evadehub/main/autoRevive.lua"))()
